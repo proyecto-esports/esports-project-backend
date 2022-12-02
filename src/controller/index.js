e605bf48164c34d614471c24fd6e53d980182918
DaniR1011 <recio.dani10@gmail.com>
2022-12-02 10:00:44 +0100

    create and delete bid

--- src/controller/index.js
+++ src/controller/index.js
@@ -5,5 +5,6 @@ import * as bid from '../domain/services/service-bid.js';
 const router = express.Router();
 
 router.post('/bids', bid.Create)
+router.delete('/bids/:id', bid.Delete)
 
 export default router;
